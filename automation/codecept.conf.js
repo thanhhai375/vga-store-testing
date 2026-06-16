ed7a227f7ba758db05d3f929f01212c96688ef69
Nguyen Tan Vinh <nguyentanvinh0403@gmail.com>
2026-06-16 11:00:55 +0700

    add config  codeceptjs

---
+++ automation/codecept.conf.js
@@ -0,0 +1,25 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './E2E/**/*_test.js',
+  output: './E2E/output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './E2E/steps_file.js'
+  },
+  name: 'automation'
+}
