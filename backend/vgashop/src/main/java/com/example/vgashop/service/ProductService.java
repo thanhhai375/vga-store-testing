3cb5e8b905bb7bdd0872eab52e5174af87111123
Nguyen Tan Vinh <nguyentanvinh0403@gmail.com>
2026-06-19 22:42:13 +0700

    KCPM-123: fix VGA-Product-User

--- backend/vgashop/src/main/java/com/example/vgashop/service/ProductService.java
+++ backend/vgashop/src/main/java/com/example/vgashop/service/ProductService.java
@@ -100,29 +100,14 @@ public class ProductService {
 
         // Process
 
-        // if (keyWord == null) {
-        //     keyWord = "";
-        // }
-
-        // if (minPrice == null) {
-        //     minPrice = 0.0;
-        // }
-
-        // if (maxPrice == null) {
-        //     maxPrice = Double.MAX_VALUE;
-        // }
-
         keyWord = (keyWord == null) ? "" : keyWord;
         minPrice = (minPrice == null) ? 0.0 : minPrice;
         maxPrice = (maxPrice == null) ? Double.MAX_VALUE : maxPrice;
 
-        // Sort sort;
-
-        // if (direction.equalsIgnoreCase("desc")) {
-        //     sort = Sort.by(sortBy).descending();
-        // } else {
-        //     sort = Sort.by(sortBy).ascending();
-        // }
+        // Additional validation for price range
+        if (minPrice > maxPrice) {
+            throw new IllegalArgumentException("Khoảng giá minPrice không được lớn hơn maxPrice");
+        }
 
         Sort sort = direction.equalsIgnoreCase("desc")
         ? Sort.by(sortBy).descending() : Sort.by(sortBy).ascending();
